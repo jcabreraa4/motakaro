d2b210364eea933d6c757186d012d4398562d65f
Jorge Cabrera <jcabreraa4@proton.me>
2026-04-19 12:39:25 +0200

    Improve auth UI

--- apps/clients/app/(auth)/layout.tsx
+++ apps/clients/app/(auth)/layout.tsx
@@ -1,16 +1,38 @@
 import { SquareThemeButton } from '@workspace/ui/custom/theme-buttons';
+import { LightRays } from '@workspace/ui/magicui/light-rays';
 import { Branding } from '@/components/motakaro/branding';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <main className="flex h-screen items-center justify-center gap-6 bg-primary-foreground px-5 lg:px-0 dark:bg-[#0A0A0A]">
-      <div className="fixed top-0 left-0 z-50 p-5">
-        <Branding />
-      </div>
-      <div className="fixed top-0 right-0 z-50 p-5">
-        <SquareThemeButton className="hidden sm:flex" />
-      </div>
-      {children}
+    <main className="flex h-svh bg-primary-foreground dark:bg-[#0A0A0A]">
+      <section className="relative flex h-full w-full items-center justify-center p-5">
+        <div className="absolute top-0 left-0 p-5">
+          <Branding className="xl:hidden" />
+          <SquareThemeButton className="hidden xl:flex" />
+        </div>
+        {children}
+      </section>
+      <section className="pointer-events-none relative hidden h-full w-full border-l select-none xl:flex">
+        <div className="absolute z-10 flex h-full w-full flex-col items-center justify-center gap-4 text-center">
+          <span className="text-xs font-semibold tracking-[0.35em] uppercase">CLIENT ACCESS</span>
+          <h1 className="text-5xl font-bold text-foreground">Motakaro</h1>
+        </div>
+        <LightRays
+          blur={75}
+          speed={10}
+          count={12}
+          length="160vh"
+          color="#007fd8"
+        />
+        <LightRays
+          fromBottom
+          blur={75}
+          speed={10}
+          count={12}
+          length="160vh"
+          color="#007fd8"
+        />
+      </section>
     </main>
   );
 }
