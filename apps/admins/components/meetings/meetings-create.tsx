7b29bf572ffbb153e5f265253e090eaa0605090a
Jorge Cabrera <jcabreraa4@proton.me>
2026-08-30 15:33:16 +0200

    Update

--- apps/admins/components/meetings/meetings-create.tsx
+++ apps/admins/components/meetings/meetings-create.tsx
@@ -1,14 +1,3 @@
-import { cloneElement, useEffect } from 'react';
-
-import { getCalApi } from '@calcom/embed-react';
-
 export function MeetingsCreate({ children }: { children: React.ReactElement }) {
-  useEffect(() => {
-    (async function () {
-      const cal = await getCalApi({ namespace: 'discovery' });
-      cal('ui', { theme: 'dark', cssVarsPerTheme: { light: { 'cal-brand': '#007fd8' }, dark: { 'cal-brand': '#007fd8' } }, hideEventTypeDetails: false, layout: 'month_view' });
-    })();
-  }, []);
-
-  return cloneElement(children as React.ReactElement);
+  return <></>;
 }
